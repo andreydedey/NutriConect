116f19ed48ad0135c7e67cd254668e3741366416
Andrey Oliveira <dedey.cardoso@gmail.com>
2024-07-04 16:32:05 -0300

    templates created, static folder, and now developing the login page

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,10 @@
+import os
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, "login/index.html")
+
+
+def register(request):
+    pass
