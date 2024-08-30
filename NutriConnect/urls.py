9688928efc14d26a28390159efb7271c82e85110
Andrey Oliveira <dedey.cardoso@gmail.com>
2024-08-30 18:33:05 -0300

    redirect from empty url

--- NutriConnect/urls.py
+++ NutriConnect/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='users/login', permanent=False)),  # Redireciona a URL raiz para 'users/'
     path('admin/', admin.site.urls),
     path('users/', include("Users.urls")),
     path('home/', include("home.urls")),
